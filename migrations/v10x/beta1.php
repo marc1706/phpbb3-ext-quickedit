836b97b8491b7454480ca4fae841a838d0e38653
Marc Alexander <admin@m-a-styles.de>
2014-08-15 10:16:05 +0200

    [ticket/23] Revert changes to forum flags on deleting data

--- migrations/v10x/beta1.php
+++ migrations/v10x/beta1.php
@@ -11,6 +11,9 @@ namespace marc\quickedit\migrations\v10x;
 
 class beta1 extends \phpbb\db\migration\migration
 {
+	/** @var int quickedit forums flag */
+	const QUICKEDIT_FLAG = 128;
+
 	public function effectively_installed()
 	{
 		return isset($this->config['allow_quick_edit']);
@@ -48,5 +51,28 @@ class beta1 extends \phpbb\db\migration\migration
 		return array(
 			array('config.add', array('allow_quick_edit', 0)),
 		);
+	}
+
+	public function revert_data()
+	{
+		return array(
+			array('custom', array(array($this, 'revert_forum_flags'))),
+		);
+	}
+
+	public function revert_forum_flags()
+	{
+		$sql = 'SELECT forum_id, forum_flags FROM ' . FORUMS_TABLE . '
+				WHERE forum_flags >= ' . self::QUICKEDIT_FLAG;
+		$result = $this->db->sql_query($sql);
+
+		while ($row = $this->db->sql_fetchrow($result))
+		{
+			$sql = 'UPDATE ' . FORUMS_TABLE . '
+					SET forum_flags = ' . (($row['forum_flags'] & self::QUICKEDIT_FLAG) ? $row['forum_flags'] - self::QUICKEDIT_FLAG : $row['forum_flags']) . '
+					WHERE forum_id = ' . $row['forum_id'];
+			$this->db->sql_query($sql);
+		}
+		$this->db->sql_freeresult($result);
 	}
 }
